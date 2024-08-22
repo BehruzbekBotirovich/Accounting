db97b6c3bb8feaea61e095ba003b9c1c2926bea6
BehruzbekBotirovich <behruzbekbotirovich@gmail.com>
2024-08-22 16:36:30 +0500

    updated

---
+++ src/pages/about/components/Litsense.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Лицензии</h1>
+    <a-row :gutter="[24, 24]">
+        <a-col :lg="6" :md="8" :xm="12" v-for="img in litsenzi">
+            <a-image :src="img"></a-image>
+        </a-col>
+    </a-row>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const litsenzi = ref(['https://templates.business-in-a-box.com/imgs/1000px/license-agreement-D1180.png', 'https://templates.business-in-a-box.com/imgs/1000px/license-agreement-D1180.png', 'https://templates.business-in-a-box.com/imgs/1000px/license-agreement-D1180.png', 'https://templates.business-in-a-box.com/imgs/1000px/license-agreement-D1180.png'])
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/about/components/Recvizits.vue
@@ -0,0 +1,50 @@
+<template>
+    <a-table :columns="columns" :dataSource="data" :pagination="false" :showHeader="false" bordered>
+        <template #bodyCell="{ column, text }">
+            <span v-if="column.key === 'key'" class="font-bold" v-html="text"></span>
+            <span v-else v-html="text"></span>
+        </template>
+    </a-table>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const columns = ref([
+    {
+        title: '',
+        dataIndex: 'key',
+        key: 'key',
+    },
+    {
+        title: '',
+        dataIndex: 'value',
+        key: 'value',
+    },
+]);
+
+const data = ref([
+    {
+        key: 'Название компании',
+        value: 'ANS Resolution Uzbekistan',
+    },
+    {
+        key: 'Год основания',
+        value: '2024',
+    },
+    {
+        key: 'Тип окон',
+        value: 'Пластиковые окна',
+    },
+    {
+        key: 'Особенности',
+        value: 'Экологичность<br>Высокая теплоизоляция<br>Долговечность',
+    },
+]);
+</script>
+
+<style scoped>
+.font-bold {
+    font-weight: bold;
+}
+</style>
